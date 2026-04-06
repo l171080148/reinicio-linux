7394972c07dc643acf7ca95d5a67a455acf055d8
Antonio <a3ortegaantonio@gmail.com>
2026-04-06 17:13:53 -0600

    Dia 16 - primer script en python

---
+++ app.py
@@ -0,0 +1,5 @@
+nombre = input("Como te llamas?")
+edad= input("Que edad tienes?")
+
+print(f"hola {nombre}, bienvenido a tu reicinio en sistemas")
+print(f"tienes la edad de {edad}, buen momento para crecer")
